2ac247fbae077923f6ad14a2722bb4feedb512ed
azizshams <azizsham002@proton.me>
2025-03-31 11:28:36 +0000

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: "mongodb://july20:udrahim@cluster0-shard-00-00.3jayr.mongodb.net:27017,cluster0-shard-00-01.3jayr.mongodb.net:27017,cluster0-shard-00-02.3jayr.mongodb.net:27017/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
+  MongoURI: "mongodb+srv://azizsham0023:flpCoNCX572bajGN@cluster0.wxsfhil.mongodb.net/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
